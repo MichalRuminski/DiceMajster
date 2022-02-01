17bc62c3fe111664478c5e95cb83871ac7cd8d8b
Michał Rumiński <mihau.ruminski@gmail.com>
2022-02-01 13:54:54 +0100

    First Game code

--- Kosci/Game.cpp
+++ Kosci/Game.cpp
@@ -3,10 +3,8 @@
 
 #include <cstdlib>
 
-Game::Game(const char* p1Name, const char* p2Name, int nTurns, int mRerolls) {
-	player1Name = std::string(p1Name);
-	player2Name = std::string(p1Name);
 
+Game::Game(const char* p1Name, const char* player2Name, int nTurns, int mRerolls) {
 	numberOfTurns = nTurns;
 	maxRerolls = mRerolls;
 	currentPlayer1Reroll = currentPlayer2Reroll = 0;
@@ -19,3 +17,135 @@ Game::Game(const char* p1Name, const char* p2Name, int nTurns, int mRerolls) {
 		player1Dices[i] = player1SelectedDices[i] = player2Dices[i] = player2SelectedDices[i] = 0;
 	}
 }
+
+Game::~Game() {
+	delete[] this->player1Dices;
+	delete[] this->player2Dices;
+	delete[] this->player1SelectedDices;
+	delete[] this->player2SelectedDices;
+}
+
+void Game::nextTurn() {
+	//TODO:(Mihau) we need to clear dice selection after new turn, and after re-roll
+	this->currentTurn++;
+
+}
+
+void Game::rollDices(int pNumber) {
+	if (pNumber) {
+		for (int i = 0; i < 5; i++) {
+			if (!player2SelectedDices[i]) {
+				player2Dices[i] = rand() % 6 + 1;
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < 5; i++) {
+			if (!player1SelectedDices[i]) {
+				player1Dices[i] = rand() % 6 + 1;
+			}
+		}
+	}
+	//TODO:(Mihau) clear selection on dices here;
+
+}
+
+
+void Game::selectDice(int pNumber, int index) {
+
+	if (pNumber) {
+		if (this->player2SelectedDices[index]) {
+			player2SelectedDices[index] = 0;
+		}
+		else
+			player2SelectedDices[index] = 1;
+	}
+	else {
+		if (this->player2SelectedDices[index]) {
+			player2SelectedDices[index] = 0;
+		}
+		else
+			player2SelectedDices[index] = 1;
+	}
+
+
+}
+
+int  Game::calculateScore(int pNumber) {
+
+	throw std::exception();
+	//int numberOfValues[6] = { 0,0,0,0,0,0 };
+
+	//int indexOf2 = 0;
+	//int indexOf3 = 0;
+	//int indexOf4 = 0;
+	//int indexOf5 = 0;
+
+	//int val;
+	////check for ones, twos, threes, etc
+	//for (int i = 0; i < 5; i++) {
+	//	val = dices[i].getCurrentValue();
+	//	numberOfValues[val - 1]++;
+	//}
+	//for (int i = 0; i < 5; i++) {
+	//	if (numberOfValues[i] == 2) {
+	//		indexOf2 = i;
+	//	}
+	//	else if (numberOfValues[i] == 3)
+	//		indexOf3 = i;
+	//	else if (numberOfValues[i] == 4)
+	//		indexOf4 = i;
+	//	else if (numberOfValues[i] == 5)
+	//		indexOf5 = i;
+	//}
+
+	////set values of simple combinations
+	//possiblePoints[0] = 1 * (numberOfValues[0]);
+	//possiblePoints[1] = 2 * (numberOfValues[1]);
+	//possiblePoints[2] = 3 * (numberOfValues[2]);
+	//possiblePoints[3] = 4 * (numberOfValues[3]);
+	//possiblePoints[4] = 5 * (numberOfValues[4]);
+	//possiblePoints[5] = 6 * (numberOfValues[5]);
+
+	////multiple values
+	//possiblePoints[6] = indexOf3 ? numberOfValues[indexOf3] * (indexOf3 + 1) : 0;
+	//possiblePoints[7] = indexOf4 ? numberOfValues[indexOf4] * (indexOf4 + 1) : 0;
+	//possiblePoints[8] = indexOf5 ? numberOfValues[indexOf5] * (indexOf5 + 1) : 0;
+	//possiblePoints[9] = indexOf3 ? numberOfValues[indexOf3] * (indexOf3 + 1) +
+	//	numberOfValues[indexOf2] * (indexOf2 + 1) : 0;
+	//possiblePoints[10] = areValuesSmallSequance(numberOfValues) ? 40 : 0;
+	//possiblePoints[11] = areValuesLargeSequance(numberOfValues) ? 60 : 0;
+	//possiblePoints[12] = numberOfValues[0] + (numberOfValues[1] * 2)
+	//	+ (numberOfValues[2] * 3) + (numberOfValues[3] * 4)
+	//	+ (numberOfValues[4] * 5) + (numberOfValues[5] * 6);
+
+}
+
+bool Game::areValuesSmallSequance(int* val) {
+	if (val[0] && val[1] && val[2] && val[3] || val[1] && val[2] && val[3] && val[4] ||
+		val[2] && val[3] && val[4] && val[5])
+		return true;
+	return false;
+}
+
+bool Game::areValuesLargeSequance(int* val) {
+	if (val[0] && val[1] && val[2] && val[3] && val[4] ||
+		val[1] && val[2] && val[3] && val[4] && val[5])
+		return true;
+	return false;
+}
+
+//Game::Game(const char* p1Name, const char* p2Name, int nTurns, int mRerolls) {
+//
+//	numberOfTurns = nTurns;
+//	maxRerolls = mRerolls;
+//	currentPlayer1Reroll = currentPlayer2Reroll = 0;
+//
+//	player1Dices = new int[5];
+//	player1SelectedDices = new int[5];
+//	player2Dices = new int[5];
+//	player2SelectedDices = new int[5];
+//	for (int i = 0; i < 5; i++) {
+//		player1Dices[i] = player1SelectedDices[i] = player2Dices[i] = player2SelectedDices[i] = 0;
+//	}
+//}
--- Kosci/Game.h
+++ Kosci/Game.h
@@ -1,36 +1,49 @@
 #ifndef GAME_H
 #define GAME_H
 
+//#pragma once
 #include<string>
 
-#pragma once
 class Game
 {
 
 public:
-	Game(const char* p1Name, const char* player2Name, int nTurns = 3, int mRerolls = 2 );
 
-	int GetScore(int* dices);		//return calculated score,
-	void RollDices(int playerN);	//set random values to dicesc in array of player: playerN;
-	void SelectDice(int index);
-	int* GetDiceValues(int playerN);
+	//int GetScore(int* dices);		//return calculated score,
+	//void RollDices(int playerN);	//set random values to dicesc in array of player: playerN;
+	//void SelectDice(int index);
+	//int* GetDiceValues(int playerN);
+	Game(const char* p1Name, const char* player2Name, int nTurns = 3, int mRerolls = 2);
+	// Game();
+	~Game();
+
+	void nextTurn();
+	void rollDices(int pNumber);
+	void selectDice(int pNumber, int index);
+	int  getDiceValue(int index);
+	void updateScore();
+	int calculateScore(int pNumber); //returns points for player pNumber dices// check for combinations  if none present return sum of all values
+	bool gameOver();
+private:
+	bool areValuesSmallSequance(int* val);
+	bool areValuesLargeSequance(int* val);
 
 private:
 
 
 
 	int numberOfTurns;
+	int currentTurn;
 	int maxRerolls;
 	int currentPlayer1Reroll;
 	int currentPlayer2Reroll;
 
 	int* player1Dices;
-	int* player1SelectedDices;
+	int* player1SelectedDices;  //value 1 at index means dice is selected and should not be rerolled
 	int* player2Dices;
-	int* player2SelectedDices;
+	int* player2SelectedDices;  //value 1 at index means dice is selected and should not be rerolled
 
-	std::string player1Name;	//to consider if this is needed here
-	std::string player2Name;	//to consider if this is needed here
+
 };
 
 #endif // !Game_H
