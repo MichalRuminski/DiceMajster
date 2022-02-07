7a7ef7e209f461d107c7a7fce1324f234db92373
Michał Rumiński <mihau.ruminski@gmail.com>
2022-02-07 17:58:53 +0100

    renamed variables names, changed displayed text

--- Kosci/StartWindow.h
+++ Kosci/StartWindow.h
@@ -39,17 +39,17 @@ namespace Kosci {
 			}
 		}
 
-	private: Kosci::GameWindow^ m;
-	private: System::Windows::Forms::Label^ label1;
+	private: Kosci::GameWindow^ gameWindow;
+	private: System::Windows::Forms::Label^ label_p1Name;
 	protected:
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::ComboBox^ comboBox1;
-	private: System::Windows::Forms::ComboBox^ comboBox2;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label_p2Name;
+	private: System::Windows::Forms::Label^ label_turnNum;
+	private: System::Windows::Forms::Label^ label_rrollNum;
+	private: System::Windows::Forms::ComboBox^ comboBox_turnNum;
+	private: System::Windows::Forms::ComboBox^ comboBox_rrollsNum;
+	private: System::Windows::Forms::Button^ button_StartGame;
+	private: System::Windows::Forms::TextBox^ textBox_p1Name;
+	private: System::Windows::Forms::TextBox^ textBox_p2Name;
 
 	private:
 		/// <summary>
@@ -64,130 +64,130 @@ namespace Kosci {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
-			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label_p1Name = (gcnew System::Windows::Forms::Label());
+			this->label_p2Name = (gcnew System::Windows::Forms::Label());
+			this->label_turnNum = (gcnew System::Windows::Forms::Label());
+			this->label_rrollNum = (gcnew System::Windows::Forms::Label());
+			this->comboBox_turnNum = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox_rrollsNum = (gcnew System::Windows::Forms::ComboBox());
+			this->button_StartGame = (gcnew System::Windows::Forms::Button());
+			this->textBox_p1Name = (gcnew System::Windows::Forms::TextBox());
+			this->textBox_p2Name = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
-			// label1
+			// label_p1Name
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(62, 127);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
+			this->label_p1Name->AutoSize = true;
+			this->label_p1Name->Location = System::Drawing::Point(62, 127);
+			this->label_p1Name->Name = L"label_p1Name";
+			this->label_p1Name->Size = System::Drawing::Size(44, 13);
+			this->label_p1Name->TabIndex = 0;
+			this->label_p1Name->Text = L"Gracz 1";
 			// 
-			// label2
+			// label_p2Name
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(62, 160);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"label2";
+			this->label_p2Name->AutoSize = true;
+			this->label_p2Name->Location = System::Drawing::Point(62, 155);
+			this->label_p2Name->Name = L"label_p2Name";
+			this->label_p2Name->Size = System::Drawing::Size(44, 13);
+			this->label_p2Name->TabIndex = 1;
+			this->label_p2Name->Text = L"Gracz 2";
 			// 
-			// label3
+			// label_turnNum
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(65, 228);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(35, 13);
-			this->label3->TabIndex = 2;
-			this->label3->Text = L"label3";
+			this->label_turnNum->AutoSize = true;
+			this->label_turnNum->Location = System::Drawing::Point(65, 228);
+			this->label_turnNum->Name = L"label_turnNum";
+			this->label_turnNum->Size = System::Drawing::Size(44, 13);
+			this->label_turnNum->TabIndex = 2;
+			this->label_turnNum->Text = L"Iloœæ tur";
 			// 
-			// label4
+			// label_rrollNum
 			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(271, 228);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(35, 13);
-			this->label4->TabIndex = 3;
-			this->label4->Text = L"label4";
+			this->label_rrollNum->AutoSize = true;
+			this->label_rrollNum->Location = System::Drawing::Point(271, 228);
+			this->label_rrollNum->Name = L"label_rrollNum";
+			this->label_rrollNum->Size = System::Drawing::Size(89, 13);
+			this->label_rrollNum->TabIndex = 3;
+			this->label_rrollNum->Text = L"Iloœæ pow. rzutów";
 			// 
-			// comboBox1
+			// comboBox_turnNum
 			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"2", L"3", L"4", L"5" });
-			this->comboBox1->Location = System::Drawing::Point(107, 228);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 21);
-			this->comboBox1->TabIndex = 4;
+			this->comboBox_turnNum->FormattingEnabled = true;
+			this->comboBox_turnNum->Items->AddRange(gcnew cli::array< System::Object^  >(5) { L"3", L"4", L"5", L"6", L"7" });
+			this->comboBox_turnNum->Location = System::Drawing::Point(115, 228);
+			this->comboBox_turnNum->Name = L"comboBox_turnNum";
+			this->comboBox_turnNum->Size = System::Drawing::Size(121, 21);
+			this->comboBox_turnNum->TabIndex = 4;
 			// 
-			// comboBox2
+			// comboBox_rrollsNum
 			// 
-			this->comboBox2->FormattingEnabled = true;
-			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"2", L"3", L"4", L"5" });
-			this->comboBox2->Location = System::Drawing::Point(313, 228);
-			this->comboBox2->Name = L"comboBox2";
-			this->comboBox2->Size = System::Drawing::Size(121, 21);
-			this->comboBox2->TabIndex = 5;
+			this->comboBox_rrollsNum->FormattingEnabled = true;
+			this->comboBox_rrollsNum->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"1", L"2", L"3", L"4" });
+			this->comboBox_rrollsNum->Location = System::Drawing::Point(366, 228);
+			this->comboBox_rrollsNum->Name = L"comboBox_rrollsNum";
+			this->comboBox_rrollsNum->Size = System::Drawing::Size(121, 21);
+			this->comboBox_rrollsNum->TabIndex = 5;
 			// 
-			// button1
+			// button_StartGame
 			// 
-			this->button1->Location = System::Drawing::Point(182, 283);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(168, 54);
-			this->button1->TabIndex = 6;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &StartWindow::button1_Click);
+			this->button_StartGame->Location = System::Drawing::Point(182, 283);
+			this->button_StartGame->Name = L"button_StartGame";
+			this->button_StartGame->Size = System::Drawing::Size(168, 54);
+			this->button_StartGame->TabIndex = 6;
+			this->button_StartGame->Text = L"START";
+			this->button_StartGame->UseVisualStyleBackColor = true;
+			this->button_StartGame->Click += gcnew System::EventHandler(this, &StartWindow::button_StartGame_Click);
 			// 
-			// textBox1
+			// textBox_p1Name
 			// 
-			this->textBox1->Location = System::Drawing::Point(107, 127);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 7;
+			this->textBox_p1Name->Location = System::Drawing::Point(107, 127);
+			this->textBox_p1Name->Name = L"textBox_p1Name";
+			this->textBox_p1Name->Size = System::Drawing::Size(100, 20);
+			this->textBox_p1Name->TabIndex = 7;
 			// 
-			// textBox2
+			// textBox_p2Name
 			// 
-			this->textBox2->Location = System::Drawing::Point(107, 152);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
-			this->textBox2->TabIndex = 8;
+			this->textBox_p2Name->Location = System::Drawing::Point(107, 152);
+			this->textBox_p2Name->Name = L"textBox_p2Name";
+			this->textBox_p2Name->Size = System::Drawing::Size(100, 20);
+			this->textBox_p2Name->TabIndex = 8;
 			// 
 			// StartWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(572, 407);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->comboBox2);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox_p2Name);
+			this->Controls->Add(this->textBox_p1Name);
+			this->Controls->Add(this->button_StartGame);
+			this->Controls->Add(this->comboBox_rrollsNum);
+			this->Controls->Add(this->comboBox_turnNum);
+			this->Controls->Add(this->label_rrollNum);
+			this->Controls->Add(this->label_turnNum);
+			this->Controls->Add(this->label_p1Name);
+			this->Controls->Add(this->label_p2Name);
 			this->Name = L"StartWindow";
-			this->Text = L"StartWindow";
+			this->Text = L"Koœci";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &StartWindow::StartWindow_FormClosing);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void button_StartGame_Click(System::Object^ sender, System::EventArgs^ e) {
 		//create game
 		//create GameWindow (main.h)
 		//show GameWindow and hide this window(dont know if this will work)
-		System::String^ selectedValue1 = (System::String^)(this->comboBox1)->SelectedItem;
-		System::String^ selectedValue2 = (System::String^)(this->comboBox2)->SelectedItem;
+		System::String^ selectedValue1 = (System::String^)(this->comboBox_turnNum)->SelectedItem;
+		System::String^ selectedValue2 = (System::String^)(this->comboBox_rrollsNum)->SelectedItem;
 
-		Game* g = new Game(marshal_as<std::string>(this->textBox1->Text), marshal_as<std::string>(this->textBox2->Text));
+		Game* g = new Game(marshal_as<std::string>(this->textBox_p1Name->Text), marshal_as<std::string>(this->textBox_p2Name->Text));
 
-		m = gcnew GameWindow(g,this);
-		m->Show();
+		this->gameWindow = gcnew GameWindow(g,this);
+		this->gameWindow->Show();
 		this->Hide();
-		if (m == nullptr) {
+		if (this->gameWindow == nullptr) {
 			delete g;
 			this->Show();
 		}
